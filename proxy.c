1c600a4df6c448eb0c30099c152ca2fc1a18a95f
wkanchan <wkanchan@carpetshark.ics.cs.cmu.edu>
2013-04-22 14:09:26 -0400

    implementing part 1

--- proxy.c
+++ proxy.c
@@ -1,15 +1,134 @@
 #include <stdio.h>
+#include <string.h>
+#include "csapp.h"
 
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
-static const char *user_agent = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
-static const char *accept = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
-static const char *accept_encoding = "Accept-Encoding: gzip, deflate\r\n";
+#define PORT 4647
 
-int main()
+static const char *msg_user_agent = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *msg_accept = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
+static const char *msg_accept_encoding = "Accept-Encoding: gzip, deflate\r\n";
+
+void doit(int connfd);
+void read_requesthdrs(rio_t *rp);
+void clienterror(int connfd, char *cause, char *errnum,
+    char *shortmsg, char *longmsg);
+int parse_uri(char *uri, char *hostname, char *path);
+
+int main(int argc, char **argv)
 {
-    printf("%s%s%s", user_agent, accept, accept_encoding);
+    int listenfd, connfd, port;
+    socklen_t clientlen;
+    struct sockaddr_in clientaddr;
+    struct hostent *hp;
+    char *haddrp;
+
+	// if (argc != 1) {
+	// 	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	// 	exit(0);
+	// }
+    printf("%s%s%s", msg_user_agent, msg_accept, msg_accept_encoding);
+	port = (argc != 2)?PORT:atoi(argv[1]);
+    printf("Running proxy at port %d..\n", port);
+
+    /* Listen to incoming clients.. forever */
+    listenfd = Open_listenfd(port);
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+
+        /* Show information of connected client */
+        hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+            sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        haddrp = inet_ntoa(clientaddr.sin_addr);
+        printf("server connected to %s (%s)\n", hp->h_name, haddrp);
+
+        doit(connfd);
+        Close(connfd);
+    }
+
+    return 0;
+}
+
+
+void doit(int connfd) {
+    // struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE],
+        hostname[MAXLINE], path[MAXLINE];
+    rio_t rio;
+
+    /* Read request line and headers */
+    Rio_readinitb(&rio, connfd);
+    Rio_readlineb(&rio, buf, MAXLINE);
+    sscanf(buf, "%s %s %s", method, uri, version);
+    printf("<< %s", buf);
+    /* Only support GET */
+    if (strcasecmp(method, "GET")) {
+        clienterror(connfd, method, "501", "Not Implemented",
+            "This proxy doesn't implement this method.");
+        return;
+    }
+    if (parse_uri(uri, hostname, path)) {
+        clienterror(connfd, method, "501", "Unrecognizable URI",
+            "This proxy doesn't know how to recognize this URI.");
+        return;
+    }
+    printf("* Hostname:%s Path:%s\n",hostname,path);
+
+    read_requesthdrs(&rio);
+
+
+}
+
+/* read_requesthdrs - Read the rest of header
+    But we ignore them */
+void read_requesthdrs(rio_t *rp) {
+    char buf[MAXLINE];
+
+    do {
+        Rio_readlineb(rp, buf, MAXLINE);
+        printf("<< %s", buf);
+    } while (strcmp(buf, "\r\n"));
+}
+
+void clienterror(int connfd, char *cause, char *errnum,
+    char *shortmsg, char *longmsg) {
+    char buf[MAXLINE], body[MAXBUF];
+
+    /* Build the HTTP response body */
+    sprintf(body, "<html><title>Proxy Error</title>");
+    sprintf(body, "%s<h1>%s: %s</h1>\r\n", body, errnum, shortmsg);
+    sprintf(body, "%s<p>%s: %s</p>\r\n", body, longmsg, cause);
+    sprintf(body, "%s</html>\r\n", body);
+
+    /* Print the HTTP response */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(connfd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    Rio_writen(connfd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+    Rio_writen(connfd, buf, strlen(buf));
+    Rio_writen(connfd, body, strlen(body));
+
+}
+
+int parse_uri(char *uri, char *hostname, char *path) {
+    char *ptr_begin, *ptr_end;
+    int hostname_length = 0;
+
+    /* Get hostname */
+    ptr_begin = strstr(uri, "://");
+    ptr_begin += 3;
+    ptr_end = strchr(ptr_begin, '/');
+    hostname_length = (int)(ptr_end-ptr_begin);
+    strncpy(hostname, ptr_begin, hostname_length);
+    hostname[hostname_length] = '\0';
+
+    /* Get path */
+    ptr_begin = ptr_end;
+    strncpy(path, ptr_begin, strlen(uri));
 
     return 0;
 }
